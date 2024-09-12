39b53ee9a4fdf0007100c960ddf44ce14c18a797
konstantin <konstabarilo12@gmail.com>
2024-09-12 13:26:18 +0300

    feat(server): sign in

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { SignUpDto } from "./dto/sign-up.dto";
 import { UsersService } from "src/users/users.service";
 import { JwtService } from "@nestjs/jwt";
+import { SignInDto } from "./dto/sign-in.dto";
+import * as argon2 from "argon2";
 
 @Injectable()
 export class AuthService {
@@ -24,5 +26,26 @@ export class AuthService {
                 password: user.password,
             }),
         };
+    }
+
+    public async signIn(signInDto: SignInDto) {
+        const user = await this.usersService.findOneByEmail(signInDto.email);
+
+        if (!user) {
+            throw new NotFoundException(`User with email="${signInDto.email} not found.`);
+        }
+
+        const passwordsMatch = await argon2.verify(user.password, signInDto.password);
+        if (passwordsMatch) {
+            return {
+                access: this.jwtService.sign({
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    password: user.password,
+                }),
+            };
+        }
+        throw new BadRequestException("Password incorrect");
     }
 }
